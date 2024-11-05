adbaafdae7e003438e14278b0b641019282c088b
PremiumBrownBook <esther.annezo--sebire@student.isae-supaero.fr>
2024-11-05 11:44:57 +0100

    determining why 1 2 3 do not work

--- engine.py
+++ engine.py
@@ -1,3 +1,10 @@
+# -*- coding: utf-8 -*-
+
+
+#created by Matthieu Couturier and Esther Annézo--Sébire
+# Different Muscl scheme
+# ============================================
+
 import numpy as np
 import misc as mi
 
@@ -128,6 +135,36 @@ class MUSCL():
         flux = mi.fillGhosts(flux)
         return flux
     
+    def fillFlux1_MUSCL_Laney(self, w, f, phi):
+        N = w.shape[0]
+        flux = np.empty(N)
+
+        for j in range(2, N-2):
+            if (w[j+1] - w[j]) == 0:
+                riL = np.inf
+            else:
+                riL = (w[j] - w[j-1])/(w[j+1] - w[j])
+            if (w[j+2] - w[j+1]) == 0:
+                riR = np.inf
+            else:
+                riR  = (w[j+1] - w[j])/(w[j+2] - w[j+1])
+
+            uL = w[j] + 0.5 * phi(riL) * (w[j+1] - w[j])
+            uR = w[j+1] - 0.5 * phi(riR) * (w[j+2] - w[j+1])
+
+            fL = f(uL)
+            fR = f(uR)
+
+            aL = self.jac(uL)
+            aR = self.jac(uR)
+
+            a = max(abs(aL), abs(aR))
+
+            flux[j] = 0.5 * (fL + fR) - 0.5 * a * (uR - uL)
+
+        flux = mi.fillGhosts(flux)
+        return flux
+    
     def fillDamp(self,w):
         N = w.shape[0]
         damp = np.empty(N)
@@ -168,11 +205,45 @@ class MUSCL():
         Dw = np.empty((u0w.shape[0]))
 
         for _ in range(Nt):
-            F0w = self.fillFlux0(u0w, self.flux, phiMinMod)
-            F1w = self.fillFlux1(u0w, self.flux, phiMinMod)
+            F0w = self.fillFlux0(u0w, self.flux, phiSuperbee)
+            F1w = self.fillFlux1(u0w, self.flux, phiSuperbee)
             Dw = self.fillDamp(u0w)
 
-            u1w[2:-2] = u0w[2:-2] - self.nu * (F1w[2:-2] - F0w[2:-2]) # + 0.1*self.dt/self.dx/self.dx*Dw[2:-2]
+            k1 = -self.nu * (F1w[2:-2] - F0w[2:-2]) #RK4
+            u1 = u0w[2:-2] + 0.5 * k1
+            u1_w = mi.addGhosts(u1, num_of_ghosts=2)
+            u1_w = mi.fillGhosts(u1_w, num_of_ghosts=2)
+            
+            F0w_1 = self.fillFlux0(u1_w, self.flux, phiMinMod)
+            F1w_1 = self.fillFlux1(u1_w, self.flux, phiMinMod)
+            k2 = -self.nu * (F1w_1[2:-2] - F0w_1[2:-2])
+            u2 = u0w[2:-2] + 0.5 * k2
+            u2_w = mi.addGhosts(u2, num_of_ghosts=2)
+            u2_w = mi.fillGhosts(u2_w, num_of_ghosts=2)
+            
+            F0w_2 = self.fillFlux0(u2_w, self.flux, phiMinMod)
+            F1w_2 = self.fillFlux1(u2_w, self.flux, phiMinMod)
+            k3 = -self.nu * (F1w_2[2:-2] - F0w_2[2:-2])
+            u3 = u0w[2:-2] + k3
+            u3_w = mi.addGhosts(u3, num_of_ghosts=2)
+            u3_w = mi.fillGhosts(u3_w, num_of_ghosts=2)
+            
+            F0w_3 = self.fillFlux0(u3_w, self.flux, phiMinMod)
+            F1w_3 = self.fillFlux1(u3_w, self.flux, phiMinMod)
+            k4 = -self.nu * (F1w_3[2:-2] - F0w_3[2:-2])
+            
+            u1w[2:-2] = u0w[2:-2] + (k1 + 2*k2 + 2*k3 + k4) / 6
+
+            #u1w[2:-2] = u0w[2:-2] - self.nu * (F1w[2:-2] - F0w[2:-2]) # + 0.1*self.dt/self.dx/self.dx*Dw[2:-2]
+
+            # u_half = u0w[2:-2] - 0.5 * self.nu * (F1w[2:-2] - F0w[2:-2]) #RK2
+            # u_half_w = mi.addGhosts(u_half, num_of_ghosts=2)
+            # u_half_w = mi.fillGhosts(u_half_w, num_of_ghosts=2)
+            
+            # F0w_half = self.fillFlux0(u_half_w, self.flux, phiMinMod)
+            # F1w_half = self.fillFlux1(u_half_w, self.flux, phiMinMod)
+            
+            # u1w[2:-2] = u0w[2:-2] - self.nu * (F1w_half[2:-2] - F0w_half[2:-2])
 
             u1w = mi.fillGhosts(u1w,num_of_ghosts=2)
 
